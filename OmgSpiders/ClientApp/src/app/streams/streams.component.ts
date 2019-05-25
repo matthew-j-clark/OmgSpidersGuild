a39dee860987b7c039a10ce85da0fc19ef89403c
Matthew Clark <matthew.j.clark@live.com>
2019-05-25 15:16:58 -0700

    Tom makes the code less dogshit.

--- OmgSpiders/ClientApp/src/app/streams/streams.component.ts
+++ OmgSpiders/ClientApp/src/app/streams/streams.component.ts
@@ -1,58 +1,26 @@
-import { Component } from '@angular/core';
-import { DomSanitizer,SafeResourceUrl } from '@angular/platform-browser'
+import { Component } from "@angular/core";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
+import * as _ from 'lodash';
 @Component({
-  selector: 'app-streams',
-  templateUrl: './streams.component.html'
+  selector: "app-streams",
+  templateUrl: "./streams.component.html"
 
 })
 export class StreamsComponent {
-  public streamPairList: StreamPair[];
-
-  constructor(private sanitized: DomSanitizer) {
-    // refactor to random order
-    this.streamPairList = [];
-    var streamsList =["SealSlicer","Glovery",
-                 "cjcox2056","rahmune",
-                 "Fancyvoidboy","madmonkie44"];
-    
-    streamsList = shuffle(streamsList);
-
-    var streamsFinal = [];
-    for (var i = 0; i < streamsList.length; i+=2) {
-      this.streamPairList[i / 2] = new StreamPair(sanitized, streamsList[i], i + 1<streamsList.length? streamsList[i + 1]:undefined);
-    }
-}
-}
-
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+  streams:SafeResourceUrl[];
   
-  return array;
-}
-class StreamPair {
-  first : SafeResourceUrl;
-  second : SafeResourceUrl;
-  constructor(private sanitized: DomSanitizer, firstEntry, secondEntry) {
-    this.first = this.createUrl(firstEntry);
-    if (secondEntry !== undefined) {
-      this.second = this.createUrl(secondEntry);
-    }
-  }
-  createUrl(entry:string) {
-    return this.sanitized.bypassSecurityTrustResourceUrl('https://player.twitch.tv/?channel=' + entry + '&muted=true');
-  }
-}
+  constructor(private readonly sanitized: DomSanitizer) {
+    
+    let streamsList = [
+      "SealSlicer", "Glovery",
+      "cjcox2056", "rahmune",
+      "Fancyvoidboy", "madmonkie44"
+    ];
 
+    this.streams =_.shuffle(_.map(streamsList, x=>this.createUrl(x, this.sanitized)));
+   
+  }
+  createUrl(entry: string, sanitizer:DomSanitizer) {
+    return sanitizer.bypassSecurityTrustResourceUrl('https://player.twitch.tv/?channel='+entry+'&muted=true');
+  }
+}
