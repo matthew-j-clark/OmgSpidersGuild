5b931eda8558b437573833362a330f98999342b0
Matthew Clark <matthew.j.clark@live.com>
2019-05-25 00:06:46 -0700

    update stream list

--- OmgSpiders/ClientApp/src/app/streams/streams.component.ts
+++ OmgSpiders/ClientApp/src/app/streams/streams.component.ts
@@ -9,19 +9,45 @@ export class StreamsComponent {
   public streamPairList: StreamPair[];
 
   constructor(private sanitized: DomSanitizer) {
-    this.streamPairList = [new StreamPair(sanitized, "SealSlicer","Glovery"),
-      new StreamPair(sanitized, "cjcox2056","rahmune" )];
+    // refactor to random order
+    this.streamPairList = [];
+    var streamsList =["SealSlicer","Glovery",
+                 "cjcox2056","rahmune",
+                 "Fancyvoidboy","madmonkie44"];
+    
+    streamsList = shuffle(streamsList);
+
+    var streamsFinal = [];
+    for (var i = 0; i < streamsList.length; i+=2) {
+      this.streamPairList[i / 2] = new StreamPair(sanitized, streamsList[i], i + 1<streamsList.length? streamsList[i + 1]:undefined);
+    }
+}
 }
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  
+  return array;
 }
-
 class StreamPair {
   first : SafeResourceUrl;
   second : SafeResourceUrl;
   constructor(private sanitized: DomSanitizer, firstEntry, secondEntry) {
     this.first = this.createUrl(firstEntry);
-    if (secondEntry !== "") {
+    if (secondEntry !== undefined) {
       this.second = this.createUrl(secondEntry);
     }
   }
