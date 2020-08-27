2591b451392fe3fa72177b3012b72a7e01ef2087
Matthew Clark <matthew.j.clark@live.com>
2020-08-27 15:15:05 -0700

    Add rundetails

--- SpiderSalesDatabase/SaleRunOperations/RunManager.cs
+++ SpiderSalesDatabase/SaleRunOperations/RunManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
@@ -9,27 +11,43 @@ namespace SpiderSalesDatabase.SaleRunOperations
 {
     public class RunManager
     {
+        public async Task<List<SaleRun>> ListRuns()
+        {
+            using (var ctx = new OmgSpidersDbContext())
+            {
+                return ctx.SaleRun.ToList();
+            }
+        }
+
+        public async Task<SaleRun> GetRunDetails(int runId)
+        {
+            using (var ctx = new OmgSpidersDbContext())
+            {
+                return ctx.SaleRun.Include(x => x.SaleRunParticipation).ThenInclude(x => x.Player).FirstOrDefault(x=>x.Id==runId);
+            }
+        }
+
         public async Task<int> AddRunAsync(string title, long goldAmount, string[] playerList)
         {
-            for(int idx =0; idx<playerList.Length;++idx)
+            for (int idx = 0; idx < playerList.Length; ++idx)
             {
                 playerList[idx] = playerList[idx].Trim();
             }
 
             using (var ctx = new OmgSpidersDbContext())
-            {                
-                var saleRun = new SaleRun() 
-                { 
-                    RunName = title, 
-                    GoldTotalAfterAdCut = goldAmount, 
-                    RunDate = (DateTime?)DateTime.Now, 
-                    PlayerCount=playerList.Length 
+            {
+                var saleRun = new SaleRun()
+                {
+                    RunName = title,
+                    GoldTotalAfterAdCut = goldAmount,
+                    RunDate = (DateTime?)DateTime.Now,
+                    PlayerCount = playerList.Length
                 };
 
                 ctx.SaleRun.Add(saleRun);
                 await ctx.SaveChangesAsync();
 
-                saleRun=ctx.SaleRun.Single(
+                saleRun = ctx.SaleRun.Single(
                     x => x.GoldTotalAfterAdCut == saleRun.GoldTotalAfterAdCut
                     && x.RunName == saleRun.RunName
                     && x.RunDate == saleRun.RunDate);
@@ -37,7 +55,7 @@ namespace SpiderSalesDatabase.SaleRunOperations
                 this.AddNewPlayersNoCommit(playerList, ctx);
                 await ctx.SaveChangesAsync();
                 // load the players we need now.
-                
+
                 foreach (var player in playerList)
                 {
                     var playerEntity = ctx.PlayerList.Single(x => x.PlayerName == player);
@@ -51,12 +69,12 @@ namespace SpiderSalesDatabase.SaleRunOperations
 
         private void AddNewPlayersNoCommit(string[] playerList, OmgSpidersDbContext ctx)
         {
-            var playersInDb = ctx.PlayerList.ToArray().Select(x => x.PlayerName).Intersect(playerList, StringComparer.OrdinalIgnoreCase);                               
+            var playersInDb = ctx.PlayerList.ToArray().Select(x => x.PlayerName).Intersect(playerList, StringComparer.OrdinalIgnoreCase);
 
             var missingPlayers = playerList.Except(playersInDb.Select(x => x), StringComparer.OrdinalIgnoreCase);
 
             ctx.PlayerList.AddRange(missingPlayers.Select(x => new PlayerList() { PlayerName = x }));
-            
+
         }
     }
 }
