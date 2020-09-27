40c28a734b152b64285901d71c6afc6d64ecaa97
Matthew Clark <matthew.j.clark@live.com>
2020-09-26 20:13:35 -0700

    Only enter typing state when we have a real command.

--- OmgSpiders/DiscordBot/OmgSpidersBotDriver.cs
+++ OmgSpiders/DiscordBot/OmgSpidersBotDriver.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
+
 using Discord;
 using Discord.Rest;
 using Discord.WebSocket;
@@ -21,11 +22,10 @@ namespace OmgSpiders.DiscordBot
         private Task botTask;
         internal static Dictionary<string, IBotCommand> CommandList { get; private set; }
         private CancellationToken cancellation;
-        
+
         public OmgSpidersBotDriver()
         {
             this.client = new DiscordSocketClient();
-
             this.client.Log += LogAsync;
             this.client.Ready += ReadyAsync;
             this.client.MessageReceived += MessageReceivedAsync;
@@ -41,7 +41,6 @@ namespace OmgSpiders.DiscordBot
         private Task ReadyAsync()
         {
             Console.WriteLine($"{this.client.CurrentUser} is connected!");
-
             return Task.CompletedTask;
         }
 
@@ -49,7 +48,6 @@ namespace OmgSpiders.DiscordBot
         // reading over the Commands Framework sample.
         private async Task MessageReceivedAsync(SocketMessage message)
         {
-            //(message.Channel as SocketGuildChannel).Guild.Roles.First(x => x.Name == "Banana Spider");
             // The bot should never respond to itself or another bot
             if (message.Author.Id == this.client.CurrentUser.Id || message.Author.IsBot)
             {
@@ -61,35 +59,34 @@ namespace OmgSpiders.DiscordBot
             {
                 return;
             }
-            using (message.Channel.EnterTypingState())
+
+            var bananaRole = (message.Channel as SocketGuildChannel).Guild.Roles.First(x => x.Name.Equals("Banana Spider", StringComparison.OrdinalIgnoreCase));
+            RestUserMessage messageToDelete = null;
+            IDisposable typingState = null;
+            try
             {
-                var bananaRole = (message.Channel as SocketGuildChannel).Guild.Roles.First(x => x.Name.Equals("Banana Spider", StringComparison.OrdinalIgnoreCase));
-                RestUserMessage messageToDelete = null;
-                try
+                if (CommandList.TryGetValue(commandKey, out var command))
                 {
-                    if (CommandList.TryGetValue(commandKey, out var command))
-                    {
-                        messageToDelete = await message.Channel.SendMessageAsync("Processing Command");
-                        Authorize(command, message.Author);
-                        await command.ProcessMessageAsync(message);
-                    }
-                }
-                catch(UnauthorizedCommandUsageException ex)
-                {
-                    await message.Channel.SendMessageAsync(ex.Message);
-                }
-                catch (Exception ex)
-                {
-                    
-                    await this.LogAsync(new LogMessage(LogSeverity.Error, commandKey, "exception", ex));
-                    await message.Channel.SendMessageAsync($"Error in command {message.Content}. {bananaRole.Mention} please take a look.");
-                }
-                finally
-                {
-                    await messageToDelete.DeleteAsync();
+                    typingState=message.Channel.EnterTypingState();
+                    messageToDelete = await message.Channel.SendMessageAsync("Processing Command");
+                    Authorize(command, message.Author);
+                    await command.ProcessMessageAsync(message);
                 }
             }
-
+            catch (UnauthorizedCommandUsageException ex)
+            {
+                await message.Channel.SendMessageAsync(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                await this.LogAsync(new LogMessage(LogSeverity.Error, commandKey, "exception", ex));
+                await message.Channel.SendMessageAsync($"Error in command {message.Content}. {bananaRole.Mention} please take a look.");
+            }
+            finally
+            {
+                typingState?.Dispose();
+                await messageToDelete.DeleteAsync();
+            }
         }
 
         public void Authorize(IBotCommand command, SocketUser user)
@@ -98,29 +95,29 @@ namespace OmgSpiders.DiscordBot
             var guildUser = user as SocketGuildUser;
             var authorizedAttribute = command.GetType().CustomAttributes
                 .FirstOrDefault(x =>
-                x.AttributeType== typeof(AuthorizedGroupAttribute));
-            
-            if (authorizedAttribute==null)
+                x.AttributeType == typeof(AuthorizedGroupAttribute));
+
+            if (authorizedAttribute == null)
             {
                 return;
-            }            
-            if(guildUser==null && authorizedAttribute!=null)
+            }
+            if (guildUser == null && authorizedAttribute != null)
             {
-                return ;
+                return;
             }
 
             var authorizedRoles = authorizedAttribute.ConstructorArguments[0].Value as IEnumerable<CustomAttributeTypedArgument>;
             var authorizedRoleNames = authorizedRoles.Select(arg => (string)arg.Value);
-      
+
             var userRoleNames = guildUser.Roles.Select(x => x.Name);
             var userRoleIds = guildUser.Roles.Select(x => x.Id.ToString());
 
-            var isAuth= userRoleNames.Any(name => authorizedRoleNames.Any(authRole => authRole.Equals(name, StringComparison.InvariantCultureIgnoreCase)))
-                   || userRoleIds.Any(name => authorizedRoleNames.Any(authRole => authRole.Equals(name, StringComparison.InvariantCultureIgnoreCase)));            
+            var isAuth = userRoleNames.Any(name => authorizedRoleNames.Any(authRole => authRole.Equals(name, StringComparison.InvariantCultureIgnoreCase)))
+                   || userRoleIds.Any(name => authorizedRoleNames.Any(authRole => authRole.Equals(name, StringComparison.InvariantCultureIgnoreCase)));
 
-            if(!isAuth)
+            if (!isAuth)
             {
-                throw new UnauthorizedCommandUsageException(command.StartsWithKey,authorizedRoleNames);
+                throw new UnauthorizedCommandUsageException(command.StartsWithKey, authorizedRoleNames);
             }
         }
 
@@ -133,25 +130,25 @@ namespace OmgSpiders.DiscordBot
 
         private void RegisterCommands()
         {
-            var commands = 
+            var commands =
                 from t in Assembly.GetExecutingAssembly().GetTypes()
                 where t.GetInterfaces().Contains(typeof(IBotCommand))
                       && t.GetConstructor(Type.EmptyTypes) != null
                 select Activator.CreateInstance(t) as IBotCommand;
             commands = commands.Union(GenericImageList.CommandList);
-            CommandList = commands.ToDictionary(x => x.StartsWithKey, x=>x, StringComparer.OrdinalIgnoreCase);
-            
+            CommandList = commands.ToDictionary(x => x.StartsWithKey, x => x, StringComparer.OrdinalIgnoreCase);
+
         }
 
         public void StopBot()
         {
-            
+
         }
 
         private async Task RunBot()
         {
             await this.client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("OmgSpidersBotToken"));
-            
+
             await this.client.StartAsync();
             await this.client.SetGameAsync("!spiderhelp to list commands");
             // Block the program until it is closed.
